4422434dedabce126310449794bf491198fffdb8
Oliveth Ndubuka <ndubukaoliveth@gmail.com>
2022-03-16 06:53:25 -0700

    Writes a program that prints _putchar, followed by a new line.

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,22 @@
 #include "main.h"
 
 /**
- * print_alphabet - Entry point
- * Return:0
+ * main - print _putchar
+ *
+ * Description: print _putchar
+ *
+ * Return: 0 on (Success)
  */
-void print_alphabet(void)
+int main(void)
 {
-	char c = 'a';
-	while (c <= 'z')
-	{
-		_putchar(c);
-		c++;
+	char text[9] ="_putchar';
 
+		int i = 0;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(text[i]);
 	}
 	_putchar('\n');
+	return (0);
 }
