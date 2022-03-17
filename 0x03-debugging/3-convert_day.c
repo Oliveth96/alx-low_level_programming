e9bffcc6be1bd327564a2d61de01e7b6ebd6cc7b
Oliveth Ndubuka <ndubukaoliveth@gmail.com>
2022-03-17 14:47:45 -0700

    Fixes the 'print_remaining_days' function so that the output works correctly for all dates and all leap years.

--- 0x03-debugging/3-convert_day.c
+++ 0x03-debugging/3-convert_day.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * convert_day - converts day of the month to day of the year,
- * without accounting for leap year
+ * convert_day - converts day of the month to day of the year, without accounting 
+ * for leap year
  * @month: month in number format
  * @day: day of month
  * Return: day of year
