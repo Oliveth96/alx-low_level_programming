816e25d0b0bfe4f232536247b7a9c1c3e15f5456
Oliveth Ndubuka <ndubukaoliveth@gmail.com>
2022-03-17 05:51:46 -0700

    Writes a function that adds two integers and returns the result.

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,7 +4,8 @@
  * add - check main
  * @a: An integer a
  * @b: An interger b
- * Description: Function that adds two intergers and returns the result 
+ * Description: Function that adds two intergers and returns the 
+ * result 
  * Result: Result to add a and b
  */
 int add(int a, int b)
