bcd76252dc0d1d7e31a23402421beb9876f1e028
Oliveth Ndubuka <ndubukaoliveth@gmail.com>
2022-03-17 17:45:44 -0700

    Writes a function that prints a triangle, followed by a new line

---
+++ 0x04-more_functions_nested_loops/10-print_trangle.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_triangle - pyramid of a # print to the screen given the input size
+ * @size: size of the pyramid.
+ * Return: void.
+ */
+void print_traingle(int size)
+{
+	int a, b;
+	
+	if (size <= 0)
+		_putchar('\n');
+
+	for (a = 0; a < size; a++)
+	{
+		for (b = 0; b <size; b++)
+		{
+			if (b >= (size - (a + 1)))
+				_putchar('#');
+			else
+				_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
