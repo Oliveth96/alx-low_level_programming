de4c5ef7f9b63ca68c7a18d8a0761e2a2ac33a50
Oliveth96 <79146676+Oliveth96@users.noreply.github.com>
2022-03-24 13:23:20 +0100

    Writes a function that concatenates two strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * _strncat - concatenates two strings given bit count of string to add..
- * @dest: string to add and return.
- * @src: string to concatenate onto dest.
+ * _strncat - concats two strings given bit count of string to add..
+ * @dest: string to add to and return.
+ * @src: string to concat onto dest.
  * @n: length of string to be added.
  * Return: dest.
  */
@@ -13,7 +13,7 @@ char *_strncat(char *dest, char *src, int n)
 
 	for (a = 0; dest[a] != '\0'; ++a)
 		;
-	for (b = 0; b < n && src [b] != '\0'; ++b, ++a)
+	for (b = 0; b < n && src[b] != '\0'; ++b, ++a)
 		dest[a] = src[b];
 	dest[a] = '\0';
 	return (dest);
