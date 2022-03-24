6ec34870db9e49422ea5ac982db34ea4d05cc827
Oliveth Ndubuka <ndubukaoliveth@gmail.com>
2022-03-24 02:19:50 -0700

    Writes  a function that concatenates two strings.

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,7 +12,7 @@ char *_strncat(char *dest, char *src, int n)
 	int a, b;
 
 	for (a = 0; dest[a] != '\0'; ++a)
-		:
+		;
 	for (b = 0; b < n && src [b] != '\0'; ++b, ++a)
 		dest[a] = src[b];
 	dest[a] = '\0';
