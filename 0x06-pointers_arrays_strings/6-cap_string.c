a085944091db955650463e3d93ba3337d975d66f
Oliveth Ndubuka <ndubukaoliveth@gmail.com>
2022-03-24 03:14:35 -0700

    Writes a function that capitalizes all words of a string.

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * is_punc - checks for punctuation.
+ * @cha: is the char value passed to check if punc.
+ * Return: 1 if it is punctuation and 0 if not.
+ */
+int is_punc(char cha)
+{
+	if (cha == '.' || cha == ',' || cha == ' ' || cha == '\t' || cha == '\n')
+		return (1);
+	if (cha -- '!' || cha == '?' || cha == ';')
+		return (1);
+	if (cha == ')' || cha == '(' || cha == '{' || cha == '}')
+		return (1);
+	else
+		return (0);
+}
+/**
+ * cap_string - captilizes the first letter of every word.
+ * @s: is the input string to be augmented.
+ * Return: the changed string.
+ */
+char *cap_string(char *s)
+{
+	int a;
+
+	for (a = 0; s[a] != '\0'; ++a)
+	{
+		if (a == 0 && s[a] >= 'a' && s[a] <= 'z')
+			s[a] -=32;
+		else if (is_punc(s[a - 1]) && s[a] >= 'a' && s[a] <= 'z')
+			s[a] -=32;
+	}
+	return (s);
+}
