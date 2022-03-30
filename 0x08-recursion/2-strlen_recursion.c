3730fd40e6a79a8551031e1977550dbd47a268e0
Oliveth Ndubuka <ndubukaoliveth@gmail.com>
2022-03-30 09:13:23 -0700

    Writes a function that returns the length of a string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - return string len
+ * @s: input string
+ * Description: returns the length of a string
+ * Return: returns string len
+ */
+int _strlen_recursion(char *s)
+{
+	if (!*s)
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(s + 1));
+}
