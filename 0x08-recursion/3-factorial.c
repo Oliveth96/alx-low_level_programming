94fd0f3201dd7fd9498902ab59ae4bd7bd0f6ee1
Oliveth Ndubuka <ndubukaoliveth@gmail.com>
2022-03-30 09:21:00 -0700

    Writes a function that returns the factorial of a given number.

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * factorial - print factorial
+ * @n: input number
+ * Description: returns the factorial of a given number
+ * Retrun: returns factorial
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n <= 1)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1));
+}
