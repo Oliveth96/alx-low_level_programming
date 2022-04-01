29c4c25c7b39561f23bc727632eae633f7496dff
Oliveth Ndubuka <ndubukaoliveth@gmail.com>
2022-04-01 05:59:33 -0700

    Writes a program that multiplies two numbers.

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - multiplies two arguments and prints result
+ * @arc: argument count
+ * @argv: argument vector
+ * Return: (0) if no errors
+ */
+int main(int argc, char *argv[])
+{
+	int i, first_num, sec_num, product;
+
+	if (argc != 3)
+	{
+		return (printf("Erro\n"), 1);
+	}
+	for (i = 0; i < argc; i++)
+	{
+		first_num = atoi(argv[1]);
+		sec_num = atoi(argv[2]);
+	}
+	product = first_num * sec_num;
+	printf("%d\n", product);
+	return (0);
+}
