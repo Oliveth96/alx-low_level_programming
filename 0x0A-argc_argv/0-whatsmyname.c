b8289c03b407b931f322d916e704ecc4118c89c6
Oliveth Ndubuka <ndubukaoliveth@gmail.com>
2022-04-01 05:08:33 -0700

    Writes a program that prints its name, followed by a new line.

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints file name
+ * @argc: first parameter
+ * @argv: second parameter
+ *
+ * Description: A program that prints its name
+ * Return: Always (0) success
+ */
+
+int main(int argc, char **argv)
+{
+	while (argc--)
+	{
+		printf("%s\n", *argv++);
+	}
+	return (0);
+}
