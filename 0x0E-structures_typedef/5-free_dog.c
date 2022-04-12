416821969c8f337e7567872eed0252b9e34115b5
Oliveth Ndubuka <ndubukaoliveth@gmail.com>
2022-04-11 21:38:55 -0700

    Writes a function that frees dogs

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -4,13 +4,15 @@
 /**
  * free_dog - Free dogs.
  * @d: The dog to be free.
+ *
+ * Return:.....
  */
 void free_dog(dog_t *d)
 {
-	if (d == NULL)
-		return;
-
-	free(d->owner);
-	free(d->name);
-	free(d);
+	if (d != NULL)
+	{
+		free(d->owner);
+		free(d->name);
+		free(d);
+	}
 }
