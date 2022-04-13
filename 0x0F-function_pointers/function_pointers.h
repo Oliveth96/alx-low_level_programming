a6df9028a1fde95d5f13ec124afb84b403039d8a
Oliveth96 <79146676+Oliveth96@users.noreply.github.com>
2022-04-13 22:27:09 +0100

    Writes a program that prints the opcodes of its own main function.

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,13 +1,5 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
-
-/*
- * File: function_pointers.h
- *
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x0E-function_pointers directory.
- */
-
 #include <stdlib.h>
 
 void print_name(char *name, void (*f)(char *));
