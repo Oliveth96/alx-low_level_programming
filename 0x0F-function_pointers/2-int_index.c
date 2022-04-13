dc40c62ef813e4daf3adccb9334c27d6ab9967bf
Oliveth96 <79146676+Oliveth96@users.noreply.github.com>
2022-04-13 22:28:42 +0100

    Writes a program that prints the opcodes of its own main function.

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,7 +14,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (size > 0)
 	{
-		if (array != NULL && cmp !== NULL)
+		if (array != NULL && cmp != NULL)
 		{
 			while (i < size)
 			{
