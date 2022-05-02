63bda387830c7a295520987bfe9e8e47894da72e
Oliveth96 <79146676+Oliveth96@users.noreply.github.com>
2022-05-02 14:58:33 +0100

    Writes a program that displays the information contained in the ELF header at the start of an ELF file.

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @ac: int variable
+ * @av: char variable
+ *
+ * Return: always 0 
+ */
+
+int main (int ac, char **av)
+{
+    return (0);
+}
